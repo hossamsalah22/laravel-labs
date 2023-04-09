ec22803cdc88817a36848915e092c2a2234c1a59
hossamsalah22 <hossamsalahabbas@gmail.com>
2023-04-09 13:46:38 +0200

    Fixed Bug Github && Google Login

--- config/services.php
+++ config/services.php
@@ -37,8 +37,8 @@ return [
     ],
 
     'google' => [
-        'client_id' => 'GOOGLE_CLIENT_ID',
-        'client_secret' => 'GOOGLE_CLIENT_SECRET',
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => 'http://localhost:8000/auth/google/callback',
     ],
 
